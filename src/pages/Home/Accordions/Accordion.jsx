e00d587fd8e654ee4803ec3f955f9db2835e7990
somaiya shahin <somaiyashahin484@gmail.com>
2023-06-08 03:07:21 +0600

    faq section completed

--- src/pages/Home/Accordions/Accordion.jsx
+++ src/pages/Home/Accordions/Accordion.jsx
@@ -3,42 +3,51 @@ import faqimg from "../../../assets/faq.png"
 
 const Accordion = () => {
     return (
-        <div className="mt-24">
+        <div className="mt-28">
             <Sectiontitels
                 heading={"FAQ"}
                 subheading={"Some frequently asking question"}
             ></Sectiontitels>
-            <div className="flex sm:flex-col md:flex-row gap-3 px-20">
+            <div className="flex sm:flex-col md:flex-row gap-3 px-20 justify-evenly items-center mt-6">
                 <div>
                     <div className="collapse collapse-plus bg-base-50">
                         <input type="radio" name="my-accordion-3" checked="checked" />
                         <div className="collapse-title text-xl font-medium">
-                            Click to open this one and close others
+                            Can I participate in the summer camp if I have no prior experience playing an instrument?
                         </div>
                         <div className="collapse-content">
-                            <p>hello</p>
+                            <p>Absolutely! Our summer camp welcomes students of all levels, including beginners. Our instructors will provide guidance and support tailored to your skill level, ensuring a positive learning experience.</p>
                         </div>
                     </div>
                     <div className="collapse collapse-plus bg-base-50">
                         <input type="radio" name="my-accordion-3" />
                         <div className="collapse-title text-xl font-medium">
-                            Click to open this one and close others
+                            What instruments are available for instruction at the summer camp?
                         </div>
                         <div className="collapse-content">
-                            <p>hello</p>
+                            <p>We offer a wide range of instruments for instruction, including guitar, piano, violin, drums, flute, saxophone, and more. During the registration process, you can select your instrument of choice based on availability.</p>
                         </div>
                     </div>
                     <div className="collapse collapse-plus bg-base-50">
                         <input type="radio" name="my-accordion-3" />
                         <div className="collapse-title text-xl font-medium">
-                            Click to open this one and close others
+                            Are instruments provided, or do I need to bring my own?
                         </div>
                         <div className="collapse-content">
-                            <p>hello</p>
+                            <p>While we encourage students to bring their own instruments, we do have a limited number of instruments available for rental on a first-come, first-served basis. Please indicate your need for an instrument during the registration process.</p>
+                        </div>
+                    </div>
+                    <div className="collapse collapse-plus bg-base-50">
+                        <input type="radio" name="my-accordion-3" />
+                        <div className="collapse-title text-xl font-medium">
+                            Can I choose my class schedule and duration?
+                        </div>
+                        <div className="collapse-content">
+                            <p>Yes, during registration, you will have the opportunity to select your preferred class schedule and duration. We offer flexible options to accommodate various preferences and availability. However, please note that class availability may be subject to demand.</p>
                         </div>
                     </div>
                 </div>
-                <img src={faqimg} alt="" />
+                <img className="w-[500px]" src={faqimg} alt="" />
             </div>
         </div>
     );
