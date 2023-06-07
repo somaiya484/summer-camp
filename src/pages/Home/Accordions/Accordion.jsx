1e0992aa243d8c82209294896e6b469f42e9d1d3
somaiya shahin <somaiyashahin484@gmail.com>
2023-06-08 02:46:15 +0600

    faq image added

--- src/pages/Home/Accordions/Accordion.jsx
+++ src/pages/Home/Accordions/Accordion.jsx
@@ -1,38 +1,44 @@
 import Sectiontitels from "../../../components/sectiontitels/Sectiontitels";
+import faqimg from "../../../assets/faq.png"
 
 const Accordion = () => {
     return (
         <div className="mt-24">
             <Sectiontitels
-            heading={"FAQ"}
-            subheading={"Some frequently asking question"}
+                heading={"FAQ"}
+                subheading={"Some frequently asking question"}
             ></Sectiontitels>
-            <div className="collapse collapse-plus bg-base-50">
-                <input type="radio" name="my-accordion-3" checked="checked" />
-                <div className="collapse-title text-xl font-medium">
-                    Click to open this one and close others
+            <div className="flex sm:flex-col md:flex-row gap-3 px-20">
+                <div>
+                    <div className="collapse collapse-plus bg-base-50">
+                        <input type="radio" name="my-accordion-3" checked="checked" />
+                        <div className="collapse-title text-xl font-medium">
+                            Click to open this one and close others
+                        </div>
+                        <div className="collapse-content">
+                            <p>hello</p>
+                        </div>
+                    </div>
+                    <div className="collapse collapse-plus bg-base-50">
+                        <input type="radio" name="my-accordion-3" />
+                        <div className="collapse-title text-xl font-medium">
+                            Click to open this one and close others
+                        </div>
+                        <div className="collapse-content">
+                            <p>hello</p>
+                        </div>
+                    </div>
+                    <div className="collapse collapse-plus bg-base-50">
+                        <input type="radio" name="my-accordion-3" />
+                        <div className="collapse-title text-xl font-medium">
+                            Click to open this one and close others
+                        </div>
+                        <div className="collapse-content">
+                            <p>hello</p>
+                        </div>
+                    </div>
                 </div>
-                <div className="collapse-content">
-                    <p>hello</p>
-                </div>
-            </div>
-            <div className="collapse collapse-plus bg-base-50">
-                <input type="radio" name="my-accordion-3" />
-                <div className="collapse-title text-xl font-medium">
-                    Click to open this one and close others
-                </div>
-                <div className="collapse-content">
-                    <p>hello</p>
-                </div>
-            </div>
-            <div className="collapse collapse-plus bg-base-50">
-                <input type="radio" name="my-accordion-3" />
-                <div className="collapse-title text-xl font-medium">
-                    Click to open this one and close others
-                </div>
-                <div className="collapse-content">
-                    <p>hello</p>
-                </div>
+                <img src={faqimg} alt="" />
             </div>
         </div>
     );
