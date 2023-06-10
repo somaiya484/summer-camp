6a85ad60fc9c55746289c4479e064385300b2ac8
somaiya shahin <somaiyashahin484@gmail.com>
2023-06-10 18:24:51 +0600

    tanstack query installed

--- src/main.jsx
+++ src/main.jsx
@@ -7,13 +7,20 @@ import {
 } from "react-router-dom";
 import { router } from './Routes/Routes.jsx';
 import AuthProvider from './authProvider/AuthProvider';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <div className='max-w-screen-xl mx-auto bg-[#ffdcb54d]'>
-        <RouterProvider router={router} />
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div className='max-w-screen-xl mx-auto bg-[#ffdcb54d]'>
+          <RouterProvider router={router} />
+        </div>
+      </QueryClientProvider>
     </AuthProvider>
   </React.StrictMode>,
 )
