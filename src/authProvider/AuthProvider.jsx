f078bab40dff6aa737cf6156505503dbd664de88
somaiya shahin <somaiyashahin484@gmail.com>
2023-06-11 12:15:54 +0600

    set token in local storage

--- src/authProvider/AuthProvider.jsx
+++ src/authProvider/AuthProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { app } from "../firebase/firebase.config"
+import axios from "axios";
 export const AuthContext = createContext(null);
 
 const auth = getAuth(app);
@@ -39,6 +40,18 @@ const AuthProvider = ({ children }) => {
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
+
+            if (currentUser) {
+                axios.post('http://localhost:5000/jwt', { email: currentUser.email })
+                    .then(data => {
+                        console.log(data.data.token)
+                        localStorage.setItem('access-token', data
+                            .data.token)
+                    })
+            }
+            else{
+                localStorage.removeItem('access-token')
+            }
             console.log('current user', currentUser);
             setLoading(false);
         });
